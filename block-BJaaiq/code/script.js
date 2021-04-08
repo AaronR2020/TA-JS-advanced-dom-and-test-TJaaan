b7d7473381f61ab76bd1887ec0b82bcc3e730090
Aaron Rebelo <beingbetter21@gmail.com>
2021-04-08 16:13:24 +0530

    COmpleted

--- block-BJaaiq/code/script.js
+++ block-BJaaiq/code/script.js
@@ -1,19 +1,11 @@
-console.log("testing");
-
+console.log("testingA");
 
 let root=document.querySelector(".quotes");
 let max=4;
 let index=0;
-document.addEventListener("scroll",()=>{
-   let scrollTop=document.documentElement.scrollTop;
-   let scrollHeight=document.documentElement.scrollHeight;
-   let clientHeight=document.documentElement.clientHeight;
-    if(scrollTop+clientHeight>=scrollHeight){
-        addQuotes();
-    }
-});
 
 function addQuotes(){
+    console.log("testingB");//not entering
     for(let i=0;i<max;i++){
         let li=document.createElement("li");
         let blockquote=document.createElement("blockquote");
@@ -21,11 +13,21 @@ function addQuotes(){
         blockquote.innerHTML=quotes[index].quoteText;
         cite.innerHTML=quotes[index].quoteAuthor;
         index++;
+        max++;
         li.append(blockquote,cite)
-        root.append(li);
-        
+        root.append(li);   
     }
 }
+document.addEventListener('scroll',()=>{
+   let scrollTop=document.documentElement.scrollTop;
+   let scrollHeight=document.documentElement.scrollHeight;
+   let clientHeight=document.documentElement.clientHeight;
+  if(scrollTop+clientHeight>=scrollHeight){
+        addQuotes();
+    }
+});
+
+
 
 
 
