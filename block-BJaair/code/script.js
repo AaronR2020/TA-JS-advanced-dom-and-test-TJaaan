403b9ccb94eb458ef6669d2a197427b575632f47
Aaron Rebelo <beingbetter21@gmail.com>
2021-04-09 11:24:43 +0530

    done

--- block-BJaair/code/script.js
+++ block-BJaair/code/script.js
@@ -1,51 +1,102 @@
-let form=document.querySelector("form");
-let ul=document.querySelector("ul");
-let cardsData=JSON.parse(localStorage.getItem('cards'));
+let form=document.querySelector('form');
+let ul=document.querySelector('ul');
 
-form.addEventListener('submit',(event)=>{
+//collection of data
+let collection=JSON.parse(localStorage.getItem('cards'))||[];
+
+
+form.addEventListener('submit', event => {
     event.preventDefault();
-    let title=event.target.element.title.value;
-    let catigory=event.target.element.title.catigory;
-    cardsData.push({title,catigory});
-    localStorage.setItem('cards',JSON.stringify(cardsData));
-    createUI(cardsData,ul);
-});
+   let title=event.target.elements.title.value;
+   let catigory=event.target.elements.catigory.value
+   collection.push({title,catigory});
+   localStorage.setItem('cards',JSON.stringify(collection))
+   createUI(collection,ul)
+ });
 
-function handleEdit(event,info,id,lable){
+ function createUI(collection,root=ul){
+     root.innerText='';
+       /*
+    basic aproach is everytime you run a forEach loop>
+     you take in the values of title and catigory>
+     and you append it to the DOM.. this is expensive
+     so you can use documentFragment();
+     here we append everything to documentFragment() and then 
+     we append documentFragment to the root.
+     so instead of dom getting manupilated n times it will get changed 
+     only once
+    */
+    let fragment=new DocumentFragment();
+//collection is a array of objects so run a forEach on it:)
+collection.forEach((tile,index)=>{
+    //each tile is one collection of {title,catigory}
+    let li=document.createElement('li');
+    let p=document.createElement('p');
+    p.addEventListener('dblclick',(event)=>replaceC(tile.catigory,event,index))
+    p.innerText=tile.catigory;
+    let h2=document.createElement('h2');
+    h2.innerText=tile.title;
+    h2.addEventListener('dblclick',(event)=>replaceT(tile.catigory,event,index))
+    li.append(p,h2)
+    fragment.appendChild(li)
+})
+    root.append(fragment)
+ }
+
+
+ function replaceC(catig,event,index){
+    //get element you want to replace
     let elem=event.target;
-    let input=document.createElement('input');
-    input.value=info;
-    input.addEventListener('keyup',(e)=>{
-        if(e.keyCode===13){
-            let updatedValue=e.target.value;
-            cardsData[id][lable]=updatedValue;
-            createUI();
-            localStorage.setItem('cards',JSON.stringify(cardsData))
-        }
-    });
+    //create input element
+    let input=document.createElement('input')
+
+    //replace text with input element 
     let parent=event.target.parentElement;
     parent.replaceChild(input,elem);
-}
-function createUI(date=cardsData,root=ul){
-root.innerHTML="";
-let fragment=new DocumentFragment();
-DataCue.forEach((cardInfo,index)=>{
-    let li=document.createElement("li");
-    let p=document.createElement('p');
-    p.addEventListener('dblclick',(e)=>{
-        handleEdit(e,cardInfo,catigory,index,catigory);
-        p.innerText=cardsInfo.catigory;
-        
-        
-    })
-    let h2=document.createElement('h2');
-    h2.addEventListener('dblclick',(e)=>{
-        handleEdit(e,cardInfo,catigory,index,catigory);
 
+    //add current value in input element
+    input.value=catig;//placeholder
+
+    input.addEventListener("keyup",(event)=>{
+        if(event.keyCode===13){
+           let newVal= event.target.value;
+           console.log(newVal);
+           //update collection with the new value
+           collection[index].catigory=newVal;
+           //call createUi again
+           createUI(collection,ul);
+        }
     })
-    li.append(p,h2);
-    fragment.appendChild(li);
-    root.append(fragment);
-})
+
+
+console.log(event.target.innerText);
 }
-createUI(cardsData,ul)
+
+function replaceT(catig,event,index){
+    //get element you want to replace
+    let elem=event.target;
+    //create input element
+    let input=document.createElement('input')
+
+    //replace text with input element 
+    let parent=event.target.parentElement;
+    parent.replaceChild(input,elem);
+
+    //add current value in input element
+    input.value=catig;//placeholder
+
+    input.addEventListener("keyup",(event)=>{
+        if(event.keyCode===13){
+           let newVal= event.target.value;
+           console.log(newVal);
+           //update collection with the new value
+           collection[index].title=newVal;
+           //call createUi again
+           createUI(collection,ul);
+        }
+    })
+
+
+console.log(event.target.innerText);
+}
+ createUI(collection,ul)
