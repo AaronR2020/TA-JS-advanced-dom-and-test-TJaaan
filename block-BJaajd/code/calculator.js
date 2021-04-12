1e774e0f371b1947e50c85d5fa819575f2b15d89
AaronR <beingbetter21@gmail.com>
2021-04-12 11:31:03 +0530

    factorial not working

--- block-BJaajd/code/calculator.js
+++ block-BJaajd/code/calculator.js
@@ -1,4 +1,56 @@
 function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function sum(a,b) {
+  return a + b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function modulo(a,b) {
+  return a % b;
+}
+
+module.exports.add = add;
+module.exports.subtract = subtract;
+module.exports.sum = sum;
+module.exports.multiply = multiply;
+module.exports.divide = divide;
+module.exports.modulo = modulo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function add(a,b) {
   return a+b;
 }
 
@@ -44,3 +96,4 @@ module.exports = {
   factorial,
   removeFromArray
 };
+ */
